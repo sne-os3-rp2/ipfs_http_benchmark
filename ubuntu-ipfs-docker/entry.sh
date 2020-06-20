04810c6e1de8a1b4c9a493d8493a4005bdee11d1
daderemi <daderemi@amiens.studlab.os3.nl>
2020-06-21 01:46:10 +0200

    Adding the ubuntu docker image with ipfs, nginx installed

---
+++ ubuntu-ipfs-docker/entry.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+# TODO see if this file can be removed
+#ipfs init --profile=badgerds
+
+#ipfs key gen publish-key
+
+#ipfs daemon --migrate=true --enable-namesys-pubsub --enable-pubsub-experiment &
+
+
+exec "$@"
