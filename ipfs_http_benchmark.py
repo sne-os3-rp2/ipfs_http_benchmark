b5ecbc2bb5f5cbfc0beb1efa7445944fdc36f04a
Woudt <woudt@steenbergen.dev>
2020-06-23 16:51:22 +0200

    Revisited topology. Included rudimentary implementation of generating files

--- ipfs_http_benchmark.py
+++ ipfs_http_benchmark.py
@@ -13,13 +13,15 @@ setLogLevel('info')
 parser = argparse.ArgumentParser()
 parser.add_argument("-c", "--count", help="Number of nodes", default=1)
 parser.add_argument("-s", "--size", help="filesize", default="10M")
-parser.add_argument("-d", "--delay", help="delay between node hosting data and switch", default="500ms")
-parser.add_argument("-b", "--bandwidth", help="bandwidth between node hosting data and switch", default=1)
+parser.add_argument("-d", "--delay", help="delay between nodes and switch", default="500ms")
+parser.add_argument("-b", "--bandwidth", help="bandwidth between all nodes and switch", default=1)
 parser.add_argument("-t", "--type", help="type of benchmark to run", default="http")
+parser.add_argument("-ds", "--delayserver", help="delay between node hosting data and switch", default="1500ms")
 args = parser.parse_args()
 count = int(args.count)
 file_size = args.size
-delay = args.delay
+delay = args.delay #General delay between all hosts
+delay_server = args.delayserver 
 bw = int(args.bandwidth)
 t_type = args.type 
 
@@ -32,42 +34,45 @@ info('*** Adding docker containers\n')
 
 info('*** Adding switches\n')
 s1 = net.addSwitch('s1')
-s2 = net.addSwitch('s2')
+#s2 = net.addSwitch('s2')
 
-net.addLink(s1, s2, cls=TCLink, delay='100ms', bw=2)
+#net.addLink(s1, s2, cls=TCLink, delay='1500ms', bw=2)
 
 docks = []
 for i in range(1, count+1):
     d_node = f'd{i}'
 
-    docks.append(net.addDocker(d_node, ip=f'192.168.1.{i}', dimage="ubuntu-ipfs", ports=[80], mem_limit="1g"))
+    info(f'*** Adding container {d_node}.\n')
+    docks.append(net.addDocker(d_node, ip=f'192.168.1.{i}/24', dimage="ubuntu-ipfs", ports=[80], mem_limit="1g"))
 
-    if (i % 2) == 0:
-        info(f'*** Linking node {d_node} to switch {s1}  \n')
-        net.addLink(s1, net[d_node], cls=TCLink)
+    #if (i % 2) == 0:
+    #    info(f'*** Linking node {d_node} to switch {s1}  \n')
+    #    net.addLink(s1, net[d_node], cls=TCLink)
+    #else:
+    if (i == 1):
+       # linking node hosting download data to switch
+       info(f'*** Linking node hosting download data (d1) to switch with delay of {delay} and bandwidth of {bw} \n')
+       net.addLink(s1, net[d_node], cls=TCLink, delay=delay_server, bw=bw)
+
+       if (t_type == "http"): # Configure HTTP server
+            # starting ngnix on d1
+            info('*** Installing nginx on d1\n')
+            net[d_node].cmd('sudo /etc/init.d/nginx start')
+            # generating data on http server
+            info(f'*** Generating file of size {file_size} at /usr/share/nginx/html/data.txt  \n')
+            net[d_node].cmd(f'sudo truncate -s {file_size} /usr/share/nginx/html/data.txt')
+
+            #net.addLink(net[d_node], s2, cls=TCLink)
+       elif (t_type == "ipfs"): # Configure IPFS "server". File origin.
+            # generating data on first ipfs node
+            info(f'*** Generating file of size {file_size} at /tmp/data.txt  \n')
+            net[d_node].cmd(f'sudo truncate -s {file_size} /tmp/data.txt')
+            net[d_node].cmd(f'sudo dd if=/dev/urandom of=/tmp/data.txt bs=4096 count=128000')
+       else:
+            print("unexpected experiment type")
     else:
-        if i == 1:
-            if (t_type == "http"):
-               # starting ngnix on d1
-               info('*** Installing nginx on d1\n')
-               net[d_node].cmd('sudo /etc/init.d/nginx start')
-               # generating data on http server
-               info(f'*** Generating file of size {file_size} at /usr/share/nginx/html/data.txt  \n')
-               net[d_node].cmd(f'sudo truncate -s {file_size} /usr/share/nginx/html/data.txt')
-
-               # linking node hosting download data to switch
-               info(f'*** Linking node hosting download data (d1) to switch with delay of {delay} and bandwidth of {bw} \n')
-               net.addLink(net[d_node], s2, cls=TCLink, delay=delay, bw=bw)
-               #net.addLink(net[d_node], s2, cls=TCLink)
-            elif (t_type == "ipfs"):
-               # generating data on first ipfs node
-               info(f'*** Generating file of size {file_size} at /tmp/data.txt  \n')
-               net[d_node].cmd(f'sudo truncate -s {file_size} /tmp/data.txt')
-            else:
-                print("unexpected experiment type")
-        else:
-           info(f'*** Linking switch {s2} and node {d_node}  \n')
-           net.addLink(s2, net[d_node], cls=TCLink)
+        info(f'*** Linking switch {s1} and node {d_node}  \n')
+        net.addLink(s1, net[d_node],delay=delay, cls=TCLink, bw=bw)
 
 info('*** Starting network\n')
 net.start()
@@ -82,7 +87,7 @@ if (t_type == "ipfs"):
 
       net[d_node].cmd('ipfs init --profile=badgerds')
       if d_node == 'd1':
-         info('*** Adding generated date to ipfs\n') 
+         info('*** Adding generated data to ipfs\n') 
          cid = net[d_node].cmd('ipfs add -Q /tmp/data.txt').rstrip()
          print(f"*** CID of data is {cid}")
          info(f'*** Starting ipfs for node d1\n')
@@ -130,7 +135,7 @@ def run_http():
     
       d_node = f'd{i}'
     
-      result = net[d_node].cmd(f"time -p wget -q http://172.17.0.2/data.txt -P /tmp/http/data.txt; sync")
+      result = net[d_node].cmd(f"time -p wget -q http://192.168.1.1/data.txt -P /tmp/http/data.txt; sync")
       time.sleep(2)
       info(f'*** Finished data retrieval via http on node {d_node} \n')
     
@@ -144,8 +149,10 @@ def run_http():
       f.write(f"d{i},'http','{file_size}','{real}','{user}','{sys}'\n")
 
 if (t_type == "http"):
+    info('*** Starting HTTP benchmark run')
     run_http()
 elif (t_type == "ipfs"):
+    info('*** Starting IPFS benchmark run')
     run_ipfs()
 else:
     exit("wrong experiment type")
