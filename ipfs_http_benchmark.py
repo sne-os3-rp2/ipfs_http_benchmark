79d66a62f3383c7c53573c42671e35b27c023830
Woudt <woudt@steenbergen.dev>
2020-06-25 17:53:38 +0200

    HTTPS IS WORKING. Currently fetches entire files.

--- ipfs_http_benchmark.py
+++ ipfs_http_benchmark.py
@@ -15,12 +15,11 @@ parser.add_argument("-c", "--count", help="Number of nodes", default=1)
 parser.add_argument("-s", "--size", help="filesize in MiB; e.g. 10", default="10")
 parser.add_argument("-d", "--delay", help="delay between nodes and switch", default="50ms")
 parser.add_argument("-b", "--bandwidth", help="bandwidth between all nodes and switch", default=1000)
-parser.add_argument("-t", "--type", help="type of benchmark to run", default="http")
+parser.add_argument("-t", "--type", help="type of benchmark to run", default="https")
 parser.add_argument("-ds", "--delayserver", help="delay between node hosting data and switch", default="150ms")
 args = parser.parse_args()
 count = int(args.count)
 file_size = int(args.size) * 256 # Convert MiB to chunks of 4096 bytes
-info(f'file size = {file_size}\n args.size = {args.size} \n')
 delay = args.delay #General delay between all hosts
 delay_server = args.delayserver 
 bw = int(args.bandwidth)
@@ -45,7 +44,7 @@ for i in range(1, count+1):
     bootnode = True if i == 1 else False
 
     info(f'*** Adding container {d_node}.\n')
-    docks.append(net.addDocker(d_node, ip=f'192.168.1.{i}/24', dimage="ubuntu-ipfs", ports=[80], #mem_limit="1g",
+    docks.append(net.addDocker(d_node, ip=f'192.168.1.{i}/24', dimage="ubuntu-ipfs:latest", ports=[80], #mem_limit="1g",
         environment={
             "ENV_SWARM_KEY":"09b7fe038a241d5e38650b0f1811933644d6195814f863902d44698fa38b8cfa",
             "BOOTNODE_IP":"192.168.1.1",
@@ -57,12 +56,12 @@ for i in range(1, count+1):
        info(f'*** Linking node hosting download data (d1) to switch with delay of {delay} and bandwidth of {bw} \n')
        net.addLink(s1, net[d_node], cls=TCLink, delay=delay_server, bw=bw)
 
-       if (t_type == "http"): # Configure HTTP server
+       if (t_type == "https"): # Configure HTTPS server
             # starting ngnix on d1
             info('*** Installing nginx on d1\n')
             net[d_node].cmd('sudo /etc/init.d/nginx start')
             # generating data on http server
-            info(f'*** Generating file of size {file_size} at /usr/share/nginx/html/data.txt for HTTP \n')
+            info(f'*** Generating file of size {file_size} at /usr/share/nginx/html/data.txt for HTTPS \n')
             net[d_node].cmd(f'sudo dd if=/dev/urandom of=/usr/share/nginx/html/data.txt bs=4096 count={file_size}')
 
             #net.addLink(net[d_node], s2, cls=TCLink)
@@ -93,7 +92,7 @@ if (t_type == "ipfs"):
         if d_node == 'd1':
            time.sleep(2)
            info('*** Adding generated data to ipfs\n')
-           garbo = net[d_node].cmd('echo "ravioli"')
+           garbo = net[d_node].cmd('echo "ravioli"') # "flush" stdout on host
            cid = net[d_node].cmd('ipfs add -Q /tmp/data.txt').rstrip()
            print(f"*** CID of data is {cid}")
 
@@ -102,6 +101,7 @@ if (t_type == "ipfs"):
         # Change bootstrap node:
         #net[d_node].cmd('ipfs shutdown')
         net[d_node].cmd('ipfs bootstrap rm --all')
+        garbolo = net[d_node].cmd('echo "fermioli"') # used to filter out stdout from verbose errors
         interm = net[d_node].cmd(f'ipfs bootstrap add /ip4/192.168.1.1/tcp/4001/p2p/{peerid}')
         info(f'*** bootstrap command result: {interm}')
         net[d_node].cmd(f'echo "/key/swarm/psk/1.0.0/\n/base16/" > ~/.ipfs/swarm.key')
@@ -114,7 +114,6 @@ if (t_type == "ipfs"):
 
 f = open("output.csv", "w")
 f.write("'node','type','filesize','server_delay','delay','bandwidth','real','user','sys'\n")
-CLI(net)
 time.sleep(10)
 # perform retrieval
 def run_ipfs():
@@ -144,7 +143,7 @@ def run_http():
     
       d_node = f'd{i}'
     
-      result = net[d_node].cmd(f"time -p wget -q http://192.168.1.1/data.txt -P /tmp/http/data.txt; sync")
+      result = net[d_node].cmd(f"time -p wget --ca-certificate=/etc/ssl/issuer.crt -q https://192.168.1.1/data.txt -P /tmp/http/data.txt; sync")
       time.sleep(2)
       info(f'*** Finished data retrieval via http on node {d_node} \n')
     
@@ -155,10 +154,10 @@ def run_http():
       real = splitted[length-4].split(" ")[1].rstrip()
       user = splitted[length-3].split(" ")[1].rstrip()
       sys  = splitted[length-2].split(" ")[1].rstrip()
-      f.write(f"'d{i}','http','{int(args.size)}','{delay_server}','{delay}','{bw}','{real}','{user}','{sys}'\n")
+      f.write(f"'d{i}','https','{int(args.size)}','{delay_server}','{delay}','{bw}','{real}','{user}','{sys}'\n")
 
-if (t_type == "http"):
-    info('*** Starting HTTP benchmark run')
+if (t_type == "https"):
+    info('*** Starting HTTPS benchmark run')
     run_http()
 elif (t_type == "ipfs"):
     info('*** Starting IPFS benchmark run')
@@ -168,7 +167,7 @@ else:
 
 
 #info('*** Running CLI\n')
-#CLI(net)
+CLI(net)
 info('*** Finished tests!')
 info('*** Stopping network')
 
